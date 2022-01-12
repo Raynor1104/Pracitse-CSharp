0de2d1d632657156602936fead4be0b34cf38b03
Your Name <ntri1104@gmail.com>
2022-01-12 10:04:43 +0700

    add c# practise exersise

---
+++ Lowercase/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.Write("Enter a string: ");
+string? str = Console.ReadLine();
+if (str != null)
+{
+    Console.WriteLine(str.ToLower());
+}
+
+
