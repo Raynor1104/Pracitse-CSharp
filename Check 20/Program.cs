9a658228007084308ccc5284785bf37e2df33954
Your Name <ntri1104@gmail.com>
2022-01-17 16:00:08 +0700

    add c# practise exersise

---
+++ Check 20/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x, y;
+
+Console.Write("Enter first number: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+y = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(x == 20 || y == 20 || x + y == 20);
