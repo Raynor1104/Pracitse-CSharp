e8f61b19b80279f279ae8090dc055eeee5149254
Your Name <ntri1104@gmail.com>
2022-01-18 15:13:13 +0700

    add c# practise exersise

---
+++ Possitive number multiples 3 or 7/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number;
+Console.Write("Enter a number: ");
+number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(number >= 0 && (number % 3 == 0 || number % 7 == 0));
