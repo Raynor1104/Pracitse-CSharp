c537d67c5104e6e2278aa1e9a13bf5b5e176d205
Your Name <ntri1104@gmail.com>
2022-01-18 15:21:54 +0700

    add c# practise exersise

---
+++ Largest and lowest values/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int a, b ,c;
+
+Console.Write("Enter first number: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter third number: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Largest of three: {Math.Max(a, (Math.Max(b, c)))}");
+Console.WriteLine($"Lowest of three: {Math.Min(a, (Math.Min(b, c)))}");
