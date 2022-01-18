549b57132060f7a683f8269a47f6aa7bf18ee47f
Your Name <ntri1104@gmail.com>
2022-01-18 15:59:31 +0700

    add c# practise exersise

---
+++ Find 30/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num1, num2;
+
+Console.Write("Enter first number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num1 == 30 || num2 == 30 || num1 + num2 == 30);
