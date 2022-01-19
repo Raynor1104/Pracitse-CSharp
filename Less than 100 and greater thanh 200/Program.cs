61b679e727caef4d9c9a42340cf939994163c6d1
Your Name <ntri1104@gmail.com>
2022-01-19 14:35:17 +0700

    add c# practise exersise

---
+++ Less than 100 and greater thanh 200/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Less_than_100_and_greater_thanh_200
+{
+    class Program
+    {
+        static void Compare (int a, int b)
+        {
+            Console.WriteLine(a < 100 && b > 200);
+        }
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+
+            Console.Write("Enter first number(<100): ");
+            firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number(>200): ");
+            secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Compare(firstNumber, secondNumber);
+        }
+    }
+}
