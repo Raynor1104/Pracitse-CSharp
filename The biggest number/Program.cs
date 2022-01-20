38f1127e2debe5b1e4915496608f1f05898b3c58
Your Name <ntri1104@gmail.com>
2022-01-20 14:34:36 +0700

    add c# practise exersise

---
+++ The biggest number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace The_biggest_number
+{
+    class Program
+    {
+        static int TheBiggestNumber(int[] a)
+        {
+            int max = a[0];
+            int i;
+
+            for (i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[] {6,4,8,1,0,2};
+            Console.WriteLine($"The biggest number: {TheBiggestNumber(a)}");
+        }
+    }
+}
